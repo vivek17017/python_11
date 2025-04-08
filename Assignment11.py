b4442d8d2c87df2d3cece4d9f62524d543925f95
Vivek Kumar <vivekgupta70509@gmail.com>
2025-04-08 18:11:42 +0530

    Add files via upload

---
+++ Assignment11.py
@@ -0,0 +1,21 @@
+"""
+#Q1
+x=input("Enter a stringn ")
+y='my' in x
+print(y)
+
+#Q2
+x=input("Enter a string ")
+y=input("Enter a string ")
+z=x is y
+print(z)
+"""
+#Q3
+x=5>10<5
+print(x)
+#Q4
+y="Red" and "White"
+print(y)
+#5
+u=True or False
+print(u)
